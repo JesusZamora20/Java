18a744f2b4468c109b4f6addd6a077938667c3d1
JesusZamora20 <jbermudezamora87@gmail.com>
2023-01-23 23:29:06 -0500

    Starting with Java: Printing hello world and variables

---
+++ HelloWorldJava/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        //declaring a variable
+        int speed;
+        //assigning a value
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Jesus Bermudez";
+        System.out.println(employeeName);
+    }
+}
