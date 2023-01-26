999b43c75404b8e79244dc71bc4ca97a51646b28
JesusZamora20 <jbermudezamora87@gmail.com>
2023-01-26 16:08:43 -0500

    Jar files and data conversion

---
+++ HelloWorldJava/src/Casting.java
@@ -0,0 +1,12 @@
+public class Casting {
+    public static void main(String[] args) {
+        double monthlyDogs = 30.0/12.0;
+        System.out.println(monthlyDogs);
+
+        //estimacion
+        int estimatedMontlhyDogs = (int) monthlyDogs;
+        System.out.println(estimatedMontlhyDogs);
+
+        System.out.println((double) 30/12);
+    }
+}
---
+++ HelloWorldJava/src/Exercises.java
@@ -0,0 +1,22 @@
+public class Exercises {
+    public static void main(String[] args) {
+        char c = 'z';
+        int d = (int) c;
+        System.out.println(d);
+
+        int i = 250;
+        long a = (long) i;
+        short b = (short) a;
+        System.out.println(a);
+        System.out.println(b);
+
+        int num = 100;
+        float iF = (float) num;
+        iF += 5000.66;
+        System.out.println(iF);
+
+        double da = 298.638;
+        long as = (long) da /25;
+        System.out.println(as);
+    }
+}
