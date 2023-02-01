10a5c879aa4b35723f227eb80215b0151d511e74
JesusZamora20 <jbermudezamora87@gmail.com>
2023-01-31 20:10:40 -0500

    Arrays

---
+++ HelloWorldJava/src/Arrays.java
@@ -0,0 +1,25 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2]; //[Filas][Columnas]
+
+        int [][][] numbers = new int[2][2][2];
+
+        androidVersions[0] = "Apple Pie";
+        androidVersions[1] = "Banana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donnut";
+
+        /*System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);*/
+
+        //foreach
+        for (String androidVersion :androidVersions) {
+            System.out.println(androidVersion);
+        }
+    }
+}
