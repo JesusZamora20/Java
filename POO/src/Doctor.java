3e21361663ad8b3cedac264ca811bcd26c7d18d6
JesusZamora20 <jbermudezamora87@gmail.com>
2023-02-21 01:06:38 -0500

    nested classes

--- POO/src/Doctor.java
+++ POO/src/Doctor.java
@@ -1,13 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     static int id = 0; //Autoincrement
-    String name;
-    String email;
-    String speciality;
-
-    /*Doctor(String name){
-        this.name = name;
-        System.out.println("Building the Doctor Object:" + name);
-    }*/
+    private String name;
+    private String email;
+    private String speciality;
 
     Doctor(String name, String speciality){
         System.out.println("The Doctor's name is: " + name);
@@ -23,5 +21,49 @@ public class Doctor {
 
     public void showId(){
         System.out.println("ID Doctor: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time){
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,5 +1,7 @@
 import javax.print.Doc;
 
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
@@ -17,13 +19,19 @@ public class Main {
         //showMenu();
 
         Doctor myDoctor = new Doctor("Jesus B. Zamora","Surgeon");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
 
-        Patient patient = new Patient("Alejandra","alejandra@mail.com");
-        patient.setHeight(1.65);
-        patient.setWeight(54.8);
-        System.out.println(patient.getHeight() + " " +patient.getWeight());
+        System.out.println(myDoctor.getAvailableAppointments());
+
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
+//        Patient patient = new Patient("Alejandra","alejandra@mail.com");
+//        patient.setHeight(1.65);
+//        patient.setWeight(54.8);
+//        System.out.println(patient.getHeight() + " " +patient.getWeight());
     }
 }
 
