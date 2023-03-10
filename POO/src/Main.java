39ca178e890c5439323e88b59d2479c4d82823e0
JesusZamora20 <jbermudezamora87@gmail.com>
2023-03-10 01:01:05 -0500

    polymorphism: overwriting toString method

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -4,15 +4,18 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Jesus B. Zamora", "Surgeon");
+        Doctor myDoctor = new Doctor("Jesus B. Zamora", "jesus@correo.com");
         myDoctor.addAvailableAppointment(new Date(), "4pm");
         myDoctor.addAvailableAppointment(new Date(), "10am");
         myDoctor.addAvailableAppointment(new Date(), "1pm");
 
+        System.out.println(myDoctor);
+
+        /*
         for (Doctor.AvailableAppointment aA : myDoctor.getAvailableAppointments()) {
             System.out.println(aA.getDate() + " " + aA.getTime());
         }
-
+        */
         Patient patient = new Patient("Juancho","juanchorois@gmail.com");
         System.out.println(patient);
     }
