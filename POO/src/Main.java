12c5f66c56e2580e4e1ccb59918c681515724bc6
JesusZamora20 <jbermudezamora87@gmail.com>
2023-03-19 01:32:44 -0500

    anonymous classes

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,4 +1,5 @@
 import model.Doctor;
+import model.ISchedulable;
 import model.Patient;
 import model.User;
 
@@ -15,6 +16,24 @@ public class Main {
 
         User user = new Doctor("yisus", "yisus@correo.com");
         user.showDataUser();
+
+        User user1 = new User("Jesuchrist", "Jesuchrist@correo.com") {
+            @Override
+            public void showDataUser() {
+                System.out.println("Doctor\n");
+                System.out.println("Hospital: Cruz verde");
+                System.out.println("Departamento: Geriatria");
+            }
+        };
+
+        user1.showDataUser();
+
+        ISchedulable iSchedulable = new ISchedulable() {
+            @Override
+            public void shedule(Date date, String time) {
+                
+            }
+        };
 
         /*
         for (model.Doctor.AvailableAppointment aA : myDoctor.getAvailableAppointments()) {
