479e623f983596a2b9cdf81979bef93680d09b6b
JesusZamora20 <jbermudezamora87@gmail.com>
2023-03-26 19:07:50 -0500

    CRUD: Creating messages

--- Mensajes_app/src/main/java/mensajes_app/MensajesDAO.java
+++ Mensajes_app/src/main/java/mensajes_app/MensajesDAO.java
@@ -1,8 +1,31 @@
 package mensajes_app;
 
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
     public static void createMessage(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
 
+        try(Connection conexion = db_connect.get_connection()) {
+            PreparedStatement ps = null;
+            try{
+                String query="INSERT INTO mensajes (mensaje, autor_mensaje) VALUES (?,?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("mensaje creado");
+            }catch(SQLException ex){
+                System.out.println(ex);
+            }
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }
     }
 
     public static void readMessage(){
--- Mensajes_app/src/main/java/mensajes_app/MensajesService.java
+++ Mensajes_app/src/main/java/mensajes_app/MensajesService.java
@@ -1,8 +1,20 @@
 package mensajes_app;
 
+import java.util.Scanner;
+
 public class MensajesService {
     public static void createMessage(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
 
+        System.out.println("Escribe tu nombre");
+        String nombre = sc.nextLine();
+
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        MensajesDAO.createMessage(registro);
     }
 
     public static void listMessages(){
