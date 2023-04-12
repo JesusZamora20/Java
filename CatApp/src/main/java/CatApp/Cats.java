7295a0386fe162229567e2e26ee53971eabeb71b
JesusZamora20 <jbermudezamora87@gmail.com>
2023-04-12 00:01:12 -0500

    Initial configuration of the project

---
+++ CatApp/src/main/java/CatApp/Cats.java
@@ -0,0 +1,40 @@
+package CatApp;
+
+public class Cats {
+    int id;
+    String url;
+    String apiKey = "live_ZIexOh7T2wh9t6W9MY8N9IlfZVKOmy9262oMOH2IEE44KcphsvPXsqejwFhOIA9s";
+    String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
---
+++ CatApp/src/main/java/CatApp/Inicio.java
@@ -0,0 +1,22 @@
+package CatApp;
+
+import javax.swing.*;
+
+public class Inicio {
+    public static void main(String[] args) {
+        int opcion_menu = -1;
+        String[] botones = {"1. ver gatos", "2. salir"};
+
+        do{
+            String opcion = (String) JOptionPane.showInputDialog(null,
+                    "Gatitos Java", "Menu principal", JOptionPane.INFORMATION_MESSAGE,
+                    null, botones, botones[0]);
+
+            for (int i = 0; i < botones.length; i++) {
+                if(opcion.equals(botones[i]))
+            }
+        } while(opcion_menu != 1);
+
+
+    }
+}
