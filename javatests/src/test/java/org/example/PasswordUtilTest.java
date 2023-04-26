03c3fd0f339f4080fd21530ed518ad5d9b07b1f6
Jesus Zamora <jbermudezamora87@gmail.com>
2023-04-25 23:59:58 -0500

    Testing with JUnit, Checking a Password's security level

---
+++ javatests/src/test/java/org/example/PasswordUtilTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+    @Test
+    public void Weak_when_its_less_8_letters(){
+        assertEquals(PasswordUtil.SecurityLevel.WEAK, PasswordUtil.assessPassword("123aa!"));
+    }
+
+    @Test
+    public void Weak_when_its_only_letters(){
+        assertEquals(PasswordUtil.SecurityLevel.WEAK, PasswordUtil.assessPassword("abcdefgh"));
+    }
+
+    @Test
+    public void Medium_when_its_letters_and_numbers(){
+        assertEquals(PasswordUtil.SecurityLevel.MEDIUM, PasswordUtil.assessPassword("abcde12345"));
+    }
+
+    @Test
+    public void Strong_when_its_letters_numbers_symbols(){
+        assertEquals(PasswordUtil.SecurityLevel.STRONG, PasswordUtil.assessPassword("abcd124@#"));
+    }
+}
