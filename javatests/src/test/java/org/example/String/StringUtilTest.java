98669ea97299ca3d14c230827b3f238f719b3994
JesusZamora20 <jbermudezamora87@gmail.com>
2023-05-06 18:41:12 -0500

    writing more functions to the isEmpty test

--- javatests/src/test/java/org/example/String/StringUtilTest.java
+++ javatests/src/test/java/org/example/String/StringUtilTest.java
@@ -9,4 +9,19 @@ public class StringUtilTest {
     public void test_string_is_not_empty() {
         assertFalse(StringUtil.isEmpty("Jesus"));
     }
+
+    @Test
+    public void empty_quote_is_string() {
+        assertTrue(StringUtil.isEmpty(""));
+    }
+
+    @Test
+    public void null_string_is_empty() {
+        assertTrue(StringUtil.isEmpty(null));
+    }
+
+    @Test
+    public void string_space_is_empty() {
+        assertTrue(StringUtil.isEmpty("   "));
+    }
 }
