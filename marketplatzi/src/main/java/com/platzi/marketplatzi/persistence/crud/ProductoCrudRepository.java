c1553aa954b365ed2fceb2d0b3e2f04049aa4cdf
JesusZamora20 <jbermudezamora87@gmail.com>
2023-05-28 13:48:33 -0500

    Implementing Query Methods

--- marketplatzi/src/main/java/com/platzi/marketplatzi/persistence/crud/ProductoCrudRepository.java
+++ marketplatzi/src/main/java/com/platzi/marketplatzi/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,13 @@ package com.platzi.marketplatzi.persistence.crud;
 import com.platzi.marketplatzi.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    // Using Query Methods
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
