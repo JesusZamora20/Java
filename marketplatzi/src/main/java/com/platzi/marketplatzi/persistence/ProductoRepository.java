0f3e23c232ce0b44ca42cfd142311db4e8c7492e
JesusZamora20 <jbermudezamora87@gmail.com>
2023-05-28 14:54:01 -0500

    Adding more methods and using the @Repository annotation

--- marketplatzi/src/main/java/com/platzi/marketplatzi/persistence/ProductoRepository.java
+++ marketplatzi/src/main/java/com/platzi/marketplatzi/persistence/ProductoRepository.java
@@ -2,10 +2,13 @@ package com.platzi.marketplatzi.persistence;
 
 import com.platzi.marketplatzi.persistence.crud.ProductoCrudRepository;
 import com.platzi.marketplatzi.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+// using the @Repository mark, we state that the class is interacting with the Database
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +22,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
